2edf3ec9d564f99e9fbb18350896fe23087df8a1
Ola x Nilsson <olani@axis.com>
2023-03-17 09:54:34 +0100

    32bit gnu types when _TIME_BITS=64 and _FILE_OFFSET_BITS=64

--- src/unix/linux_like/linux/gnu/b32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mod.rs
@@ -29,14 +29,14 @@ cfg_if! {
         pub type rlim_t = u64;
         pub type blksize_t = i64;
     } else {
-        pub type time_t = i32;
+        pub type time_t = i64;
         pub type suseconds_t = i32;
-        pub type ino_t = u32;
-        pub type off_t = i32;
-        pub type blkcnt_t = i32;
-        pub type fsblkcnt_t = ::c_ulong;
-        pub type fsfilcnt_t = ::c_ulong;
-        pub type rlim_t = c_ulong;
+        pub type ino_t = u64;
+        pub type off_t = i64;
+        pub type blkcnt_t = i64;
+        pub type fsblkcnt_t = u64;
+        pub type fsfilcnt_t = u64;
+        pub type rlim_t = u64;
         pub type blksize_t = i32;
     }
 }
