30481cd6ca6c86aa1ec702c8c86e0352f469d426
Ola x Nilsson <olani@axis.com>
2023-03-17 15:20:29 +0100

    Move struct timex_t from gnu to gnu/b*
    Import ntptimeval into gnu/b*, used by struct timex

--- src/unix/linux_like/linux/gnu/b32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mod.rs
@@ -1,5 +1,6 @@
 //! 32-bit specific definitions for linux-like values
 
+use ntptimeval;
 use pthread_mutex_t;
 
 pub type c_long = i32;
@@ -160,6 +161,43 @@ s! {
         pub sem_nsems: ::__syscall_ulong_t,
         __glibc_reserved3: ::__syscall_ulong_t,
         __glibc_reserved4: ::__syscall_ulong_t,
+    }
+
+    pub struct timex {
+        pub modes: ::c_uint,
+        pub __unused_pad1: i32,
+        pub offset: ::c_longlong,
+        pub freq: ::c_longlong,
+        pub maxerror: ::c_longlong,
+        pub esterror: ::c_longlong,
+        pub status: ::c_int,
+        pub __unused_pad2: i32,
+        pub constant: ::c_longlong,
+        pub precision: ::c_longlong,
+        pub tolerance: ::c_longlong,
+        pub time: ::timeval,
+        pub tick: ::c_longlong,
+        pub ppsfreq: ::c_longlong,
+        pub jitter: ::c_longlong,
+        pub shift: ::c_int,
+        pub __unused_pad3: i32,
+        pub stabil: ::c_longlong,
+        pub jitcnt: ::c_longlong,
+        pub calcnt: ::c_longlong,
+        pub errcnt: ::c_longlong,
+        pub stbcnt: ::c_longlong,
+        pub tai: ::c_int,
+        pub __unused1: i32,
+        pub __unused2: i32,
+        pub __unused3: i32,
+        pub __unused4: i32,
+        pub __unused5: i32,
+        pub __unused6: i32,
+        pub __unused7: i32,
+        pub __unused8: i32,
+        pub __unused9: i32,
+        pub __unused10: i32,
+        pub __unused11: i32,
     }
 }
 
