f2e555759fe5193203e42155d18d7657aa51740a
Ola x Nilsson <olani@axis.com>
2023-03-20 14:29:55 +0100

    Fix semid_ds

--- src/unix/linux_like/linux/gnu/b32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mod.rs
@@ -125,17 +125,17 @@ s! {
 
     pub struct semid_ds {
         pub sem_perm: ipc_perm,
-        #[cfg(target_arch = "powerpc")]
-        __reserved: ::__syscall_ulong_t,
+        // #[cfg(target_arch = "powerpc")]
+        // __reserved: ::__syscall_ulong_t,
         pub sem_otime: ::time_t,
-        #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
+        #[cfg(not(any(target_arch = "mips", target_arch = "powerpc", target_arch = "arm", target_arch = "x86")))]
         __reserved: ::__syscall_ulong_t,
-        #[cfg(target_arch = "powerpc")]
-        __reserved2: ::__syscall_ulong_t,
+        // #[cfg(target_arch = "powerpc")]
+        // __reserved2: ::__syscall_ulong_t,
         pub sem_ctime: ::time_t,
-        #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
-        __reserved2: ::__syscall_ulong_t,
         pub sem_nsems: ::__syscall_ulong_t,
+        #[cfg(not(any(target_arch = "mips", target_arch = "powerpc", target_arch = "arm", target_arch = "x86")))]
+        __reserved2: ::__syscall_ulong_t,
         __glibc_reserved3: ::__syscall_ulong_t,
         __glibc_reserved4: ::__syscall_ulong_t,
     }
