58f41eaabde552723efbec46318f829f7ca5ae46
Ola x Nilsson <olani@axis.com>
2023-03-20 14:33:33 +0100

    Update struct aiocb

--- src/unix/linux_like/linux/gnu/mod.rs
+++ src/unix/linux_like/linux/gnu/mod.rs
@@ -62,8 +62,8 @@ s! {
         __error_code: ::c_int,
         __return_value: ::ssize_t,
         pub aio_offset: off_t,
-        #[cfg(all(not(target_arch = "x86_64"), target_pointer_width = "32"))]
-        __unused1: [::c_char; 4],
+        // #[cfg(all(not(target_arch = "x86_64"), target_pointer_width = "32"))]
+        // __unused1: [::c_char; 4],
         __glibc_reserved: [::c_char; 32]
     }
 
