295dcffddf18fda1bbb2d854c19284857ca028d7
Ola x Nilsson <olani@axis.com>
2023-03-20 14:21:57 +0100

    gnu: Update stat structs
    Move the default stat struct from b32/mod.rs to a new file
    fnu/b32/generic.rs and add 64-bit time handling.  Some archs still
    need their own version.
    
    stat64, statfs64 and statvfs64 becomes aliases to stat, statfs and
    statfcf respectively in most cases.

--- src/unix/linux_like/linux/gnu/b64/x86_64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/x86_64/mod.rs
@@ -9,6 +9,9 @@ pub type suseconds_t = i64;
 pub type __u64 = ::c_ulonglong;
 pub type __s64 = ::c_longlong;
 
+pub type statfs64 = statfs;
+pub type statvfs64 = statvfs;
+
 s! {
     pub struct sigaction {
         pub sa_sigaction: ::sighandler_t,
@@ -112,36 +115,6 @@ s! {
         pub st_ctime: ::time_t,
         pub st_ctime_nsec: i64,
         __reserved: [i64; 3],
-    }
-
-    pub struct statfs64 {
-        pub f_type: ::__fsword_t,
-        pub f_bsize: ::__fsword_t,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_fsid: ::fsid_t,
-        pub f_namelen: ::__fsword_t,
-        pub f_frsize: ::__fsword_t,
-        pub f_flags: ::__fsword_t,
-        pub f_spare: [::__fsword_t; 4],
-    }
-
-    pub struct statvfs64 {
-        pub f_bsize: ::c_ulong,
-        pub f_frsize: ::c_ulong,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_favail: u64,
-        pub f_fsid: ::c_ulong,
-        pub f_flag: ::c_ulong,
-        pub f_namemax: ::c_ulong,
-        __f_spare: [::c_int; 6],
     }
 
     pub struct pthread_attr_t {
