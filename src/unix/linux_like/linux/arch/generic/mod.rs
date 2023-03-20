e970ab7b2d845b8d5c0e63ed9d0984c7b1e04530
Ola x Nilsson <olani@axis.com>
2023-03-20 14:21:04 +0100

    Set SO_TIMESTAMP* and SO_RCVTIMEO and SO_SNDTIMEO

--- src/unix/linux_like/linux/arch/generic/mod.rs
+++ src/unix/linux_like/linux/arch/generic/mod.rs
@@ -37,10 +37,6 @@ pub const SO_PASSCRED: ::c_int = 16;
 pub const SO_PEERCRED: ::c_int = 17;
 pub const SO_RCVLOWAT: ::c_int = 18;
 pub const SO_SNDLOWAT: ::c_int = 19;
-pub const SO_RCVTIMEO: ::c_int = 20;
-pub const SO_SNDTIMEO: ::c_int = 21;
-// pub const SO_RCVTIMEO_OLD: ::c_int = 20;
-// pub const SO_SNDTIMEO_OLD: ::c_int = 21;
 pub const SO_SECURITY_AUTHENTICATION: ::c_int = 22;
 pub const SO_SECURITY_ENCRYPTION_TRANSPORT: ::c_int = 23;
 pub const SO_SECURITY_ENCRYPTION_NETWORK: ::c_int = 24;
@@ -49,18 +45,41 @@ pub const SO_ATTACH_FILTER: ::c_int = 26;
 pub const SO_DETACH_FILTER: ::c_int = 27;
 pub const SO_GET_FILTER: ::c_int = SO_ATTACH_FILTER;
 pub const SO_PEERNAME: ::c_int = 28;
-pub const SO_TIMESTAMP: ::c_int = 29;
-// pub const SO_TIMESTAMP_OLD: ::c_int = 29;
+
+cfg_if! {
+    if #[cfg(all(target_env = "gnu",
+                 target_pointer_width = "32",
+                 any(target_arch = "arm", target_arch = "x86")))] {
+        pub const SO_TIMESTAMP: ::c_int = SO_TIMESTAMP_NEW;
+        pub const SO_TIMESTAMP_OLD: ::c_int = 29;
+        pub const SO_TIMESTAMPNS: ::c_int = SO_TIMESTAMPNS_NEW;
+        pub const SO_TIMESTAMPNS_OLD: ::c_int = 35;
+        pub const SO_TIMESTAMPING: ::c_int = SO_TIMESTAMPING_NEW;
+        pub const SO_TIMESTAMPING_OLD: ::c_int = 37;
+        pub const SO_RCVTIMEO: ::c_int = SO_RCVTIMEO_NEW;
+        pub const SO_SNDTIMEO: ::c_int = SO_SNDTIMEO_NEW;
+        pub const SO_RCVTIMEO_OLD: ::c_int = 20;
+        pub const SO_SNDTIMEO_OLD: ::c_int = 21;
+    } else {
+        pub const SO_TIMESTAMP: ::c_int = 29;
+        // pub const SO_TIMESTAMP_OLD: ::c_int = 29;
+        pub const SO_TIMESTAMPNS: ::c_int = 35;
+        // pub const SO_TIMESTAMPNS_OLD: ::c_int = 35;
+        pub const SO_TIMESTAMPING: ::c_int = 37;
+        // pub const SO_TIMESTAMPING_OLD: ::c_int = 37;
+        pub const SO_RCVTIMEO: ::c_int = 20;
+        pub const SO_SNDTIMEO: ::c_int = 21;
+        // pub const SO_RCVTIMEO_OLD: ::c_int = 20;
+        // pub const SO_SNDTIMEO_OLD: ::c_int = 21;
+    }
+}
+
 pub const SO_ACCEPTCONN: ::c_int = 30;
 pub const SO_PEERSEC: ::c_int = 31;
 pub const SO_SNDBUFFORCE: ::c_int = 32;
 pub const SO_RCVBUFFORCE: ::c_int = 33;
 pub const SO_PASSSEC: ::c_int = 34;
-pub const SO_TIMESTAMPNS: ::c_int = 35;
-// pub const SO_TIMESTAMPNS_OLD: ::c_int = 35;
 pub const SO_MARK: ::c_int = 36;
-pub const SO_TIMESTAMPING: ::c_int = 37;
-// pub const SO_TIMESTAMPING_OLD: ::c_int = 37;
 pub const SO_PROTOCOL: ::c_int = 38;
 pub const SO_DOMAIN: ::c_int = 39;
 pub const SO_RXQ_OVFL: ::c_int = 40;
@@ -94,7 +113,8 @@ cfg_if! {
     // But they may still not have those _OLD ones.
     if #[cfg(all(any(target_arch = "x86",
                      target_arch = "x86_64",
-                     target_arch = "aarch64"),
+                     target_arch = "aarch64",
+                     target_arch = "arm"),
                  not(any(target_env = "musl", target_env = "ohos"))))] {
         pub const SO_TIMESTAMP_NEW: ::c_int = 63;
         pub const SO_TIMESTAMPNS_NEW: ::c_int = 64;
