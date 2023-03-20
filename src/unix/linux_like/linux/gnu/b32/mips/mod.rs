2484bcc79d84bf594eae2595409c10361431bf29
Ola x Nilsson <olani@axis.com>
2023-03-20 14:21:57 +0100

    Update stat structs

--- src/unix/linux_like/linux/gnu/b32/mips/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mips/mod.rs
@@ -3,26 +3,25 @@ pub type wchar_t = i32;
 
 s! {
     pub struct stat64 {
-        pub st_dev: ::c_ulong,
-        st_pad1: [::c_long; 3],
+        pub st_dev: ::dev_t,
         pub st_ino: ::ino64_t,
         pub st_mode: ::mode_t,
         pub st_nlink: ::nlink_t,
         pub st_uid: ::uid_t,
         pub st_gid: ::gid_t,
-        pub st_rdev: ::c_ulong,
-        st_pad2: [::c_long; 2],
+        pub st_rdev: ::dev_t,
         pub st_size: ::off64_t,
+        pub st_blksize: ::blksize_t,
+        pub st_blocks: ::blkcnt64_t,
         pub st_atime: ::time_t,
+        __pad1: i32,
         pub st_atime_nsec: ::c_long,
         pub st_mtime: ::time_t,
+        __pad2: i32,
         pub st_mtime_nsec: ::c_long,
         pub st_ctime: ::time_t,
+        __pad3: i32,
         pub st_ctime_nsec: ::c_long,
-        pub st_blksize: ::blksize_t,
-        st_pad3: ::c_long,
-        pub st_blocks: ::blkcnt64_t,
-        st_pad5: [::c_long; 14],
     }
 
     pub struct statfs {
