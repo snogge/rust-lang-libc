2c91ad9ac28b4285528046738e4e4a36911003d9
Ola x Nilsson <olani@axis.com>
2023-03-27 14:15:18 +0200

    gnu: Update struct msqid_ds for 64-bit time

--- src/unix/linux_like/linux/gnu/b32/mips/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mips/mod.rs
@@ -124,21 +124,9 @@ s! {
 
     pub struct msqid_ds {
         pub msg_perm: crate::ipc_perm,
-        #[cfg(target_endian = "big")]
-        __glibc_reserved1: c_ulong,
         pub msg_stime: crate::time_t,
-        #[cfg(target_endian = "little")]
-        __glibc_reserved1: c_ulong,
-        #[cfg(target_endian = "big")]
-        __glibc_reserved2: c_ulong,
         pub msg_rtime: crate::time_t,
-        #[cfg(target_endian = "little")]
-        __glibc_reserved2: c_ulong,
-        #[cfg(target_endian = "big")]
-        __glibc_reserved3: c_ulong,
         pub msg_ctime: crate::time_t,
-        #[cfg(target_endian = "little")]
-        __glibc_reserved3: c_ulong,
         __msg_cbytes: c_ulong,
         pub msg_qnum: crate::msgqnum_t,
         pub msg_qbytes: crate::msglen_t,
--- src/unix/linux_like/linux/gnu/b32/powerpc/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/powerpc/mod.rs
@@ -113,10 +113,13 @@ s! {
 
     pub struct msqid_ds {
         pub msg_perm: crate::ipc_perm,
+        #[cfg(not(gnu_time64_abi))]
         __glibc_reserved1: c_uint,
         pub msg_stime: crate::time_t,
+        #[cfg(not(gnu_time64_abi))]
         __glibc_reserved2: c_uint,
         pub msg_rtime: crate::time_t,
+        #[cfg(not(gnu_time64_abi))]
         __glibc_reserved3: c_uint,
         pub msg_ctime: crate::time_t,
         __msg_cbytes: c_ulong,
