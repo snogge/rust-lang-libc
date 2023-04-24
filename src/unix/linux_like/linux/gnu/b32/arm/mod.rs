0d852c04efb704e2d7237b099a9f70896cbea8dc
Ola x Nilsson <olani@axis.com>
2023-04-24 09:56:24 +0200

    gnu: Make type statfs64 an alias of struct statfs

--- src/unix/linux_like/linux/gnu/b32/arm/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/arm/mod.rs
@@ -1,6 +1,8 @@
 pub type c_char = u8;
 pub type wchar_t = u32;
 
+pub type statfs64 = statfs;
+
 s! {
     pub struct sigaction {
         pub sa_sigaction: ::sighandler_t,
@@ -72,21 +74,6 @@ s! {
         pub st_mtime_nsec: ::c_long,
         pub st_ctime: ::time_t,
         pub st_ctime_nsec: ::c_long,
-    }
-
-    pub struct statfs64 {
-        pub f_type: ::__fsword_t,
-        pub f_bsize: ::__fsword_t,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_fsid: ::fsid_t,
-        pub f_namelen: ::__fsword_t,
-        pub f_frsize: ::__fsword_t,
-        pub f_flags: ::__fsword_t,
-        pub f_spare: [::__fsword_t; 4],
     }
 
     pub struct statvfs64 {
