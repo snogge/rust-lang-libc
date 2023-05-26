bf8d16a4e7f746cc1ea720fe6093b10b8f758baa
Andy Caldwell <andycaldwell@microsoft.com>
2023-05-26 22:49:02 +0100

    Revert extra_traits change now dirent64 is a distinguished type

--- src/unix/linux_like/linux/mod.rs
+++ src/unix/linux_like/linux/mod.rs
@@ -905,7 +905,6 @@ cfg_if! {
             }
         }
 
-        #[cfg(not(target_env = "musl"))]
         impl PartialEq for dirent64 {
             fn eq(&self, other: &dirent64) -> bool {
                 self.d_ino == other.d_ino
@@ -920,10 +919,8 @@ cfg_if! {
             }
         }
 
-        #[cfg(not(target_env = "musl"))]
         impl Eq for dirent64 {}
 
-        #[cfg(not(target_env = "musl"))]
         impl ::fmt::Debug for dirent64 {
             fn fmt(&self, f: &mut ::fmt::Formatter) -> ::fmt::Result {
                 f.debug_struct("dirent64")
@@ -936,7 +933,6 @@ cfg_if! {
             }
         }
 
-        #[cfg(not(target_env = "musl"))]
         impl ::hash::Hash for dirent64 {
             fn hash<H: ::hash::Hasher>(&self, state: &mut H) {
                 self.d_ino.hash(state);
