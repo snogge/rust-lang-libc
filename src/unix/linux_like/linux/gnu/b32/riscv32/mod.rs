a8654a981ce04f6cef6d17d809397f371de52402
Ola x Nilsson <olani@axis.com>
2023-08-29 14:26:26 +0200

    riscv32: alias stat64

--- src/unix/linux_like/linux/gnu/b32/riscv32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/riscv32/mod.rs
@@ -44,28 +44,6 @@ s! {
         __unused: [::c_int; 2usize],
     }
 
-    pub struct stat64 {
-        pub st_dev: ::dev_t,
-        pub st_ino: ::ino64_t,
-        pub st_mode: ::mode_t,
-        pub st_nlink: ::nlink_t,
-        pub st_uid: ::uid_t,
-        pub st_gid: ::gid_t,
-        pub st_rdev: ::dev_t,
-        pub __pad1: ::dev_t,
-        pub st_size: ::off64_t,
-        pub st_blksize: ::blksize_t,
-        pub __pad2: ::c_int,
-        pub st_blocks: ::blkcnt64_t,
-        pub st_atime: ::time_t,
-        pub st_atime_nsec: ::c_long,
-        pub st_mtime: ::time_t,
-        pub st_mtime_nsec: ::c_long,
-        pub st_ctime: ::time_t,
-        pub st_ctime_nsec: ::c_long,
-        __unused: [::c_int; 2],
-    }
-
     pub struct statfs {
         pub f_type: ::c_long,
         pub f_bsize: ::c_long,
