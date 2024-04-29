ae009abb20db351714d1aff2a5fd1e37fdd49e87
Ola x Nilsson <olani@axis.com>
2024-04-29 12:10:25 +0200

    gnu: Update the stat* structs for 64-bit time
    Move the stat struct from gnu/b32/mod.rs to gnu/b32/time*.rs
    
    For arm, mips, powerpc, riscv32, and x86, move stat64 to
    time32.rs and add an stat64 = stat alias to time64.rs.
    
    For sparc, do the same for statfs64 and statvfs64.

---
+++ src/unix/linux_like/linux/gnu/b32/mips/time32.rs
@@ -0,0 +1,26 @@
+use crate::prelude::*;
+
+s! {
+    pub struct stat64 {
+        pub st_dev: c_ulong,
+        st_pad1: [c_long; 3],
+        pub st_ino: crate::ino64_t,
+        pub st_mode: crate::mode_t,
+        pub st_nlink: crate::nlink_t,
+        pub st_uid: crate::uid_t,
+        pub st_gid: crate::gid_t,
+        pub st_rdev: c_ulong,
+        st_pad2: [c_long; 2],
+        pub st_size: crate::off64_t,
+        pub st_atime: crate::time_t,
+        pub st_atime_nsec: c_long,
+        pub st_mtime: crate::time_t,
+        pub st_mtime_nsec: c_long,
+        pub st_ctime: crate::time_t,
+        pub st_ctime_nsec: c_long,
+        pub st_blksize: crate::blksize_t,
+        st_pad3: c_long,
+        pub st_blocks: crate::blkcnt64_t,
+        st_pad5: [c_long; 14],
+    }
+}
--- src/unix/linux_like/linux/gnu/b32/powerpc/time32.rs
+++ src/unix/linux_like/linux/gnu/b32/powerpc/time32.rs
@@ -1,7 +1,29 @@
 use crate::prelude::*;
-use crate::{pid_t, time_t};
+use crate::{dev_t, pid_t, time_t};
 
 s! {
+    pub struct stat64 {
+        pub st_dev: dev_t,
+        pub st_ino: crate::ino64_t,
+        pub st_mode: crate::mode_t,
+        pub st_nlink: crate::nlink_t,
+        pub st_uid: crate::uid_t,
+        pub st_gid: crate::gid_t,
+        pub st_rdev: dev_t,
+        __pad2: c_ushort,
+        pub st_size: crate::off64_t,
+        pub st_blksize: crate::blksize_t,
+        pub st_blocks: crate::blkcnt64_t,
+        pub st_atime: time_t,
+        pub st_atime_nsec: c_long,
+        pub st_mtime: time_t,
+        pub st_mtime_nsec: c_long,
+        pub st_ctime: time_t,
+        pub st_ctime_nsec: c_long,
+        __glibc_reserved4: c_ulong,
+        __glibc_reserved5: c_ulong,
+    }
+
     pub struct shmid_ds {
         pub shm_perm: crate::ipc_perm,
         __glibc_reserved1: c_uint,
