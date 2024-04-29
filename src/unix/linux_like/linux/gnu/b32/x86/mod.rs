6f4fbf34f89965e230f95f67d04c2bcb0566160c
Ola x Nilsson <olani@axis.com>
2024-04-29 12:10:25 +0200

    gnu: Update the stat* structs for 64-bit time
    Move the stat struct from gnu/b32/mod.rs to gnu/b32/time*.rs
    
    For arm, mips, powerpc, riscv32, and x86, move stat64 to
    time32.rs and add an stat64 = stat alias to time64.rs.
    
    For sparc, do the same for statfs64 and statvfs64.

--- src/unix/linux_like/linux/gnu/b32/x86/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/x86/mod.rs
@@ -130,28 +130,6 @@ s! {
         __unused2: ::c_ulong,
     }
 
-    pub struct stat64 {
-        pub st_dev: ::dev_t,
-        __pad1: ::c_uint,
-        __st_ino: ::ino_t,
-        pub st_mode: ::mode_t,
-        pub st_nlink: ::nlink_t,
-        pub st_uid: ::uid_t,
-        pub st_gid: ::gid_t,
-        pub st_rdev: ::dev_t,
-        __pad2: ::c_uint,
-        pub st_size: ::off64_t,
-        pub st_blksize: ::blksize_t,
-        pub st_blocks: ::blkcnt64_t,
-        pub st_atime: ::time_t,
-        pub st_atime_nsec: ::c_long,
-        pub st_mtime: ::time_t,
-        pub st_mtime_nsec: ::c_long,
-        pub st_ctime: ::time_t,
-        pub st_ctime_nsec: ::c_long,
-        pub st_ino: ::ino64_t,
-    }
-
     pub struct statfs64 {
         pub f_type: ::__fsword_t,
         pub f_bsize: ::__fsword_t,
@@ -1110,4 +1088,14 @@ extern "C" {
     pub fn setcontext(ucp: *const ucontext_t) -> ::c_int;
     pub fn makecontext(ucp: *mut ucontext_t, func: extern "C" fn(), argc: ::c_int, ...);
     pub fn swapcontext(uocp: *mut ucontext_t, ucp: *const ucontext_t) -> ::c_int;
+}
+
+cfg_if! {
+    if #[cfg(gnu_time64_abi)] {
+        mod time64;
+        pub use self::time64::*;
+    } else {
+        mod time32;
+        pub use self::time32::*;
+    }
 }
