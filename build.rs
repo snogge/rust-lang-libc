e7f93ae5929bad236e4bde356dfb07d314d33826
Ola x Nilsson <olani@axis.com>
2024-12-09 16:28:39 +0100

    gnu: Set up the CI for _FILE_OFFSET_BITS=64
    I was not able to get the libc-tests to rebuild properly if I ran it
    twice in the same run-docker invocation.

--- build.rs
+++ build.rs
@@ -13,6 +13,8 @@ const ALLOWED_CFGS: &'static [&'static str] = &[
     "freebsd13",
     "freebsd14",
     "freebsd15",
+    // Corresponds to `_FILE_OFFSET_BITS=64` in glibc
+    "gnu_file_offset_bits64",
     // FIXME(ctest): this config shouldn't be needed but ctest can't parse `const extern fn`
     "libc_const_extern_fn",
     "libc_deny_warnings",
@@ -44,6 +46,7 @@ fn main() {
     let rustc_dep_of_std = env::var("CARGO_FEATURE_RUSTC_DEP_OF_STD").is_ok();
     let libc_ci = env::var("LIBC_CI").is_ok();
     let libc_check_cfg = env::var("LIBC_CHECK_CFG").is_ok() || rustc_minor_ver >= 80;
+    let gnu_file_offset_bits64 = env::var("RUST_LIBC_UNSTABLE_GNU_FILE_OFFSET_BITS64").is_ok();
 
     // The ABI of libc used by std is backward compatible with FreeBSD 12.
     // The ABI of libc from crates.io is backward compatible with FreeBSD 12.
@@ -84,6 +87,9 @@ fn main() {
     println!("cargo:rerun-if-env-changed=RUST_LIBC_UNSTABLE_LINUX_TIME_BITS64");
     if linux_time_bits64 {
         set_cfg("linux_time_bits64");
+    }
+    if gnu_file_offset_bits64 {
+        set_cfg("gnu_file_offset_bits64");
     }
 
     // On CI: deny all warnings
--- libc-test/build.rs
+++ libc-test/build.rs
@@ -3390,6 +3390,13 @@ fn test_vxworks(target: &str) {
     cfg.generate(src_hotfix_dir().join("lib.rs"), "main.rs");
 }
 
+fn config_gnu_bits(target: &str, cfg: &mut ctest::TestGenerator) {
+    if target.contains("gnu") && env::var("RUST_LIBC_UNSTABLE_GNU_FILE_OFFSET_BITS64").is_ok() {
+        cfg.define("_FILE_OFFSET_BITS", Some("64"));
+        cfg.cfg("gnu_file_offset_bits64", None);
+    }
+}
+
 fn test_linux(target: &str) {
     assert!(target.contains("linux"));
 
@@ -3431,6 +3438,8 @@ fn test_linux(target: &str) {
     // deprecated since glibc >= 2.29. This allows Rust binaries to link against
     // glibc versions older than 2.29.
     cfg.define("__GLIBC_USE_DEPRECATED_SCANF", None);
+
+    config_gnu_bits(target, &mut cfg);
 
     headers! { cfg:
                "ctype.h",
@@ -4540,6 +4549,7 @@ fn test_linux_like_apis(target: &str) {
     if linux || android || emscripten {
         // test strerror_r from the `string.h` header
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
         cfg.skip_type(|_| true).skip_static(|_| true);
 
         headers! { cfg: "string.h" }
@@ -4556,6 +4566,7 @@ fn test_linux_like_apis(target: &str) {
         // test fcntl - see:
         // http://man7.org/linux/man-pages/man2/fcntl.2.html
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
 
         if musl {
             cfg.header("fcntl.h");
@@ -4585,6 +4596,7 @@ fn test_linux_like_apis(target: &str) {
     if linux || android {
         // test termios
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
         cfg.header("asm/termbits.h");
         cfg.header("linux/termios.h");
         cfg.skip_type(|_| true)
@@ -4609,6 +4621,7 @@ fn test_linux_like_apis(target: &str) {
     if linux || android {
         // test IPV6_ constants:
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
         headers! {
             cfg:
             "linux/in6.h"
@@ -4640,6 +4653,7 @@ fn test_linux_like_apis(target: &str) {
         // "resolve.h" defines a `p_type` macro that expands to `__p_type`
         // making the tests for these fails when both are included.
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
         cfg.header("elf.h");
         cfg.skip_fn(|_| true)
             .skip_static(|_| true)
@@ -4659,6 +4673,7 @@ fn test_linux_like_apis(target: &str) {
     if linux || android {
         // Test `ARPHRD_CAN`.
         let mut cfg = ctest_cfg();
+        config_gnu_bits(target, &mut cfg);
         cfg.header("linux/if_arp.h");
         cfg.skip_fn(|_| true)
             .skip_static(|_| true)
