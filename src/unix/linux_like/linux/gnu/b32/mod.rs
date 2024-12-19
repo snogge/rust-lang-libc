ec7d61728073926b1449d98b1df2f45e6a36c4b1
Ola x Nilsson <olani@axis.com>
2024-12-19 14:11:40 +0100

    gnu: Adapt stat for gnu_file_offset_bits64

--- src/unix/linux_like/linux/gnu/b32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mod.rs
@@ -20,7 +20,9 @@ cfg_if! {
     if #[cfg(target_arch = "riscv32")] {
         pub type time_t = i64;
         pub type suseconds_t = i64;
-        pub type ino_t = u64;
+        type __ino_t = c_ulong;
+        type __ino64_t = u64;
+        pub type ino_t = __ino64_t;
         pub type off_t = i64;
         pub type blkcnt_t = i64;
         pub type fsblkcnt_t = u64;
@@ -30,7 +32,9 @@ cfg_if! {
     } else if #[cfg(gnu_file_offset_bits64)] {
         pub type time_t = i32;
         pub type suseconds_t = i32;
-        pub type ino_t = u64;
+        type __ino_t = c_ulong;
+        type __ino64_t = u64;
+        pub type ino_t = __ino64_t;
         pub type off_t = i64;
         pub type blkcnt_t = i64;
         pub type fsblkcnt_t = u64;
@@ -40,7 +44,9 @@ cfg_if! {
     } else {
         pub type time_t = i32;
         pub type suseconds_t = i32;
-        pub type ino_t = u32;
+        type __ino_t = c_ulong;
+        type __ino64_t = u64;
+        pub type ino_t = __ino_t;
         pub type off_t = i32;
         pub type blkcnt_t = i32;
         pub type fsblkcnt_t = c_ulong;
@@ -73,8 +79,14 @@ cfg_if! {
                 pub st_mtime_nsec: c_long,
                 pub st_ctime: crate::time_t,
                 pub st_ctime_nsec: c_long,
-                __unused4: c_long,
-                __unused5: c_long,
+
+                #[cfg(gnu_file_offset_bits64)]
+                pub st_ino: crate::ino_t,
+
+                #[cfg(not(gnu_file_offset_bits64))]
+                __glibc_reserved4: c_ulong,
+                #[cfg(not(gnu_file_offset_bits64))]
+                __glibc_reserved5: c_ulong,
             }
         }
     }
