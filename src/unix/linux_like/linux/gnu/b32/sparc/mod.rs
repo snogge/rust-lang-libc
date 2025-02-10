f211d5768868d0824f1506242c7db75d2eac5d31
Ola x Nilsson <olani@axis.com>
2025-02-10 16:35:07 +0100

    gnu: Adapt sparc statfs and statvfs for gnu_file_offset_bits64
    statfs64 and statvfs64 are aliases of statfs and statvfs respectively.

--- src/unix/linux_like/linux/gnu/b32/sparc/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/sparc/mod.rs
@@ -5,6 +5,49 @@ use crate::{off64_t, off_t};
 
 pub type wchar_t = i32;
 
+cfg_if! {
+    if #[cfg(gnu_file_offset_bits64)] {
+        pub type statfs64 = crate::statfs;
+        pub type statvfs64 = crate::statvfs;
+    }
+}
+
+cfg_if! {
+    if #[cfg(not(gnu_file_offset_bits64))] {
+        s! {
+            pub struct statfs64 {
+                pub f_type: crate::__fsword_t,
+                pub f_bsize: crate::__fsword_t,
+                pub f_blocks: u64,
+                pub f_bfree: u64,
+                pub f_bavail: u64,
+                pub f_files: u64,
+                pub f_ffree: u64,
+                pub f_fsid: crate::fsid_t,
+                pub f_namelen: crate::__fsword_t,
+                pub f_frsize: crate::__fsword_t,
+                pub f_flags: crate::__fsword_t,
+                pub f_spare: [crate::__fsword_t; 4],
+            }
+
+            pub struct statvfs64 {
+                pub f_bsize: c_ulong,
+                pub f_frsize: c_ulong,
+                pub f_blocks: u64,
+                pub f_bfree: u64,
+                pub f_bavail: u64,
+                pub f_files: u64,
+                pub f_ffree: u64,
+                pub f_favail: u64,
+                pub f_fsid: c_ulong,
+                pub f_flag: c_ulong,
+                pub f_namemax: c_ulong,
+                __f_spare: [c_int; 6],
+            }
+        }
+    }
+}
+
 s! {
     pub struct sigaction {
         pub sa_sigaction: crate::sighandler_t,
@@ -79,36 +122,6 @@ s! {
         pub st_ctime: crate::time_t,
         pub st_ctime_nsec: c_long,
         __reserved: [c_long; 2],
-    }
-
-    pub struct statfs64 {
-        pub f_type: crate::__fsword_t,
-        pub f_bsize: crate::__fsword_t,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_fsid: crate::fsid_t,
-        pub f_namelen: crate::__fsword_t,
-        pub f_frsize: crate::__fsword_t,
-        pub f_flags: crate::__fsword_t,
-        pub f_spare: [crate::__fsword_t; 4],
-    }
-
-    pub struct statvfs64 {
-        pub f_bsize: c_ulong,
-        pub f_frsize: c_ulong,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_favail: u64,
-        pub f_fsid: c_ulong,
-        pub f_flag: c_ulong,
-        pub f_namemax: c_ulong,
-        __f_spare: [c_int; 6],
     }
 
     pub struct ipc_perm {
