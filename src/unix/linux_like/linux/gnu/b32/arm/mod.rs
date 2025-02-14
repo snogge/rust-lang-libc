94da8c1d5505e362e730d1798f223681c9badccf
Ola x Nilsson <olani@axis.com>
2025-02-14 16:05:26 +0100

    fixup! gnu: Make statfs64 an alias for statfs when gnu_file_offset_bits64

--- src/unix/linux_like/linux/gnu/b32/arm/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/arm/mod.rs
@@ -27,6 +27,21 @@ cfg_if! {
                 pub st_ctime_nsec: c_long,
                 pub st_ino: crate::ino64_t,
             }
+
+            pub struct statfs64 {
+                pub f_type: crate::__fsword_t,
+                pub f_bsize: crate::__fsword_t,
+                pub f_blocks: u64,
+                pub f_bfree: u64,
+                pub f_bavail: u64,
+                pub f_files: u64,
+                pub f_ffree: u64,
+                pub f_fsid: crate::fsid_t,
+                pub f_namelen: crate::__fsword_t,
+                pub f_frsize: crate::__fsword_t,
+                pub f_flags: crate::__fsword_t,
+                pub f_spare: [crate::__fsword_t; 4],
+            }
         }
     }
 }
@@ -84,21 +99,6 @@ s! {
         __pad2: c_ushort,
         __unused1: c_ulong,
         __unused2: c_ulong,
-    }
-
-    pub struct statfs64 {
-        pub f_type: crate::__fsword_t,
-        pub f_bsize: crate::__fsword_t,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_fsid: crate::fsid_t,
-        pub f_namelen: crate::__fsword_t,
-        pub f_frsize: crate::__fsword_t,
-        pub f_flags: crate::__fsword_t,
-        pub f_spare: [crate::__fsword_t; 4],
     }
 
     pub struct statvfs64 {
