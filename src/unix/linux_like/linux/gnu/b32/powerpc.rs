aec6ae183f65e20893c093975a8e88c841944932
Ola x Nilsson <olani@axis.com>
2025-03-07 13:15:16 +0100

    fixup! gnu: Make statfs64 an alias for statfs when gnu_file_offset_bits64

--- src/unix/linux_like/linux/gnu/b32/powerpc.rs
+++ src/unix/linux_like/linux/gnu/b32/powerpc.rs
@@ -27,6 +27,21 @@ cfg_if! {
                 __glibc_reserved4: c_ulong,
                 __glibc_reserved5: c_ulong,
             }
+
+            pub struct statfs64 {
+                pub f_type: crate::__fsword_t,
+                pub f_bsize: crate::__fsword_t,
+                pub f_blocks: u64,
+                pub f_bfree: u64,
+                pub f_bavail: u64,
+                pub f_files: u64,
+                pub f_ffree: u64,
+                pub f_fsid: crate::fsid_t,
+                pub f_namelen: crate::__fsword_t,
+                pub f_frsize: crate::__fsword_t,
+                pub f_flags: crate::__fsword_t,
+                pub f_spare: [crate::__fsword_t; 4],
+            }
         }
     }
 }
@@ -85,20 +100,6 @@ s! {
         __glibc_reserved2: u64,
     }
 
-    pub struct statfs64 {
-        pub f_type: crate::__fsword_t,
-        pub f_bsize: crate::__fsword_t,
-        pub f_blocks: u64,
-        pub f_bfree: u64,
-        pub f_bavail: u64,
-        pub f_files: u64,
-        pub f_ffree: u64,
-        pub f_fsid: crate::fsid_t,
-        pub f_namelen: crate::__fsword_t,
-        pub f_frsize: crate::__fsword_t,
-        pub f_flags: crate::__fsword_t,
-        pub f_spare: [crate::__fsword_t; 4],
-    }
 
     pub struct statvfs64 {
         pub f_bsize: c_ulong,
